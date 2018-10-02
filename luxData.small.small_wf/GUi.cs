4780962877eb1394ef24be9fc879facb29327581
ll9 <llwierer@web.de>
2018-10-02 22:40:55 +0200

    cefsharp on x86

--- luxData.small.small_wf/GUi.cs
+++ luxData.small.small_wf/GUi.cs
@@ -1,4 +1,6 @@
-﻿using luxData.small.small_wf.Views;
+﻿using CefSharp;
+using CefSharp.WinForms;
+using luxData.small.small_wf.Views;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -13,10 +15,13 @@ namespace luxData.small.small_wf
 {
     public partial class GUI : Form, IView
     {
+        public ChromiumWebBrowser chromeBrowser;
+
         public GUI()
         {
             InitializeComponent();
             new Presenter.Presenter(this);
+            InitializeChromium();
         }
 
         public object DataSource
@@ -25,6 +30,18 @@ namespace luxData.small.small_wf
             {
                 DataGrid.DataSource = value;
             }
+        }
+
+        public void InitializeChromium()
+        {
+            CefSettings settings = new CefSettings();
+            // Initialize cef with the provided settings
+            Cef.Initialize(settings);
+            // Create a browser component
+            chromeBrowser = new ChromiumWebBrowser("http://ourcodeworld.com");
+            // Add it to the form and fill it to the form window.
+            GridSplitContainer.Panel1.Controls.Add(chromeBrowser);
+            chromeBrowser.Dock = DockStyle.Fill;
         }
     }
 }

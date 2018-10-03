7010e16f70f5c3e0a448ab2cdf71bc9ed62e2903
ll9 <llwierer@web.de>
2018-10-03 08:54:49 +0200

    First draft

--- luxData.small.small_wf/Utils/ProjectManager.cs
+++ luxData.small.small_wf/Utils/ProjectManager.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace luxData.small.small_wf.Utils
 {
     public interface IProjectManager
     {
+        string DefaultFolderKey { get; set; }
         string DbFolderPath { get; set; }
         string DbFilePath { get; set; }
         string ClassificationFolderPath { get; set; }
@@ -20,9 +22,58 @@ namespace luxData.small.small_wf.Utils
         bool CreateProject(string path);
         bool RestoreBackup(string path);
         bool SetDefaultClassification(string path);
+        void PersistDefaultFolder();
 
     }
-    public class ProjectManager
+
+    /// <summary>
+    /// Responsible for handling the project folder structure
+    /// </summary>
+    public class ProjectManager : IProjectManager
     {
+        public string DbFolderPath { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string DbFilePath { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string ClassificationFolderPath { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string ClassificationFilePath { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string BackupFolderPath { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string DefaultFolderKey { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public ProjectManager(string setting, string dbFolderPath, string dbName)
+        {
+            if (!LoadProject())
+            {
+                CreateProject(Application.StartupPath);
+            }
+        }
+
+        public bool CreateProject(string path)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool LoadProject()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool LoadProject(string path)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool RestoreBackup(string path)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool SetDefaultClassification(string path)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void PersistDefaultFolder()
+        {
+            throw new NotImplementedException();
+        }
     }
 }

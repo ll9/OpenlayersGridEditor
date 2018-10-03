3c9570ebef379366f6434bc4e502b468d6f5ab39
ll9 <llwierer@web.de>
2018-10-03 16:14:56 +0200

    Basic SpatialiteManger Interface

---
+++ luxData.small.small_wf/Utils/SpatialiteManager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace luxData.small.small_wf.Utils
+{
+    public interface ISpatialiteManager
+    {
+        string DbPath { get; }
+
+        void Fill(DataTable dataTable);
+        void Update(DataTable dataTable);
+    }
+
+    class SpatialiteManager
+    {
+    }
+}

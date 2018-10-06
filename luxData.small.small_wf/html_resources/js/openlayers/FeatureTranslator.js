37b3c2e4d990c7a93a58e0c588c798ce2593fb48
ll9 <llwierer@web.de>
2018-10-06 21:38:21 +0200

    Tracking translate event

--- luxData.small.small_wf/html_resources/js/openlayers/FeatureTranslator.js
+++ luxData.small.small_wf/html_resources/js/openlayers/FeatureTranslator.js
@@ -4,5 +4,12 @@
         this.translate = new ol.interaction.Translate();
 
         this.map.addInteraction(this.translate)
+        this.initEvents();
+    }
+
+    initEvents() {
+        this.translate.on('translateend', (event) => {
+            console.log(event)
+        })
     }
 }

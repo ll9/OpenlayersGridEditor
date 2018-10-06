8bfe653babe3a2fb16a37c458c97478ccf2fadec
ll9 <llwierer@web.de>
2018-10-06 23:27:30 +0200

    Getting lonlat from translate event

--- luxData.small.small_wf/html_resources/js/openlayers/FeatureTranslator.js
+++ luxData.small.small_wf/html_resources/js/openlayers/FeatureTranslator.js
@@ -10,6 +10,12 @@
     initEvents() {
         this.translate.on('translateend', (event) => {
             console.log(event)
+            let feature = event.features.item(0);
+            let id = feature.getId();
+            let lonlat = ol.proj.transform(event.coordinate, 'EPSG:3857', 'EPSG:4326');
+
+            console.log(id);
+            console.log(lonlat);
         })
     }
 }

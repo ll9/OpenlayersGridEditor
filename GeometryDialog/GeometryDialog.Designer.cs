bda39dfa9f3078476e0696ebe2f17d3062247b5c
ll9 <llwierer@web.de>
2018-10-08 21:46:21 +0200

    Basic Geometry Dialog

---
+++ GeometryDialog/GeometryDialog.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace GeometryDialog
+{
+    partial class GeometryDialog
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.CancelButton = new System.Windows.Forms.Button();
+            this.OkButton = new System.Windows.Forms.Button();
+            this.NeuButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Items.AddRange(new object[] {
+            "test",
+            "test2"});
+            this.listBox1.Location = new System.Drawing.Point(13, 13);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(194, 160);
+            this.listBox1.TabIndex = 0;
+            // 
+            // CancelButton
+            // 
+            this.CancelButton.Location = new System.Drawing.Point(141, 204);
+            this.CancelButton.Name = "CancelButton";
+            this.CancelButton.Size = new System.Drawing.Size(66, 23);
+            this.CancelButton.TabIndex = 1;
+            this.CancelButton.Text = "Abbrechen";
+            this.CancelButton.UseVisualStyleBackColor = true;
+            // 
+            // OkButton
+            // 
+            this.OkButton.Location = new System.Drawing.Point(88, 204);
+            this.OkButton.Name = "OkButton";
+            this.OkButton.Size = new System.Drawing.Size(47, 23);
+            this.OkButton.TabIndex = 2;
+            this.OkButton.Text = "OK";
+            this.OkButton.UseVisualStyleBackColor = true;
+            // 
+            // NeuButton
+            // 
+            this.NeuButton.Location = new System.Drawing.Point(13, 204);
+            this.NeuButton.Name = "NeuButton";
+            this.NeuButton.Size = new System.Drawing.Size(44, 23);
+            this.NeuButton.TabIndex = 3;
+            this.NeuButton.Text = "Neu";
+            this.NeuButton.UseVisualStyleBackColor = true;
+            // 
+            // GeometryDialog
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(224, 246);
+            this.Controls.Add(this.NeuButton);
+            this.Controls.Add(this.OkButton);
+            this.Controls.Add(this.CancelButton);
+            this.Controls.Add(this.listBox1);
+            this.Name = "GeometryDialog";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button CancelButton;
+        private System.Windows.Forms.Button OkButton;
+        private System.Windows.Forms.Button NeuButton;
+    }
+}
+

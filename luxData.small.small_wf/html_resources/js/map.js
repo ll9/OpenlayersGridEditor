86bdc338e982d813c516c329a6e94b3e1fd43720
ll9 <llwierer@web.de>
2018-10-21 17:33:02 +0200

    Basic 72dpi landscape a4 pdf

--- luxData.small.small_wf/html_resources/js/map.js
+++ luxData.small.small_wf/html_resources/js/map.js
@@ -14,3 +14,54 @@ const map = new ol.Map({
 const layerManager = new LayerManager(map);
 const interactionManager = new InteractionManager(map, layerManager.source);
 interactionManager.setInteraction(Geometry.HAND);
+
+
+var dims = {
+    a0: [1189, 841],
+    a1: [841, 594],
+    a2: [594, 420],
+    a3: [420, 297],
+    a4: [297, 210],
+    a5: [210, 148]
+};
+
+document.addEventListener("DOMContentLoaded",function(){
+    var exportButton = document.getElementById('export-pdf');
+    //here code
+
+    exportButton.addEventListener('click', function () {
+        exportButton.disabled = true;
+        document.body.style.cursor = 'progress';
+    
+        var format = 'a4'
+        var resolution = 72
+        var dim = dims[format];
+        var width = Math.round(dim[0] * resolution / 25.4);
+        var height = Math.round(dim[1] * resolution / 25.4);
+        var size = /** @type {module:ol/size~Size} */ (map.getSize());
+        var extent = map.getView().calculateExtent(size);
+    
+        map.once('rendercomplete', function (event) {
+            var canvas = event.context.canvas;
+            var data = canvas.toDataURL('image/jpeg');
+            var pdf = new jsPDF('landscape', undefined, 'a4');
+            pdf.addImage(data, 'JPEG', 0, 0, dim[0], dim[1]);
+            pdf.save('map.pdf');
+            // Reset original map size
+            map.setSize(size);
+            map.getView().fit(extent, {
+                size: size
+            });
+            exportButton.disabled = false;
+            document.body.style.cursor = 'auto';
+        });
+    
+        // Set print size
+        var printSize = [width, height];
+        map.setSize(printSize);
+        map.getView().fit(extent, {
+            size: printSize
+        });
+    
+    }, false);
+});
